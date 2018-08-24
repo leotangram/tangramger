ab42241a1a93bbc840a77c7b23c63c04037a1c78
leotangram <leotangram@gmail.com>
2018-08-23 21:55:10 -0500

    ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,16 +7,49 @@ import { User } from '../interfaces/user';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  friends: User[];
   constructor() {
-    let myUser: User = {
+    let usuario1: User = {
       nick: 'Leonardo',
+      subnick: 'Tangram',
       age: 25,
       email: 'leotangram@gmail.com',
       friend: true,
       uid: 1
     };
-    console.log(myUser);
+    let usuario2: User = {
+      nick: 'Eduardo',
+      subnick: 'Mi mensaje personal',
+      age: 28,
+      email: 'eduardo@eduardo.com',
+      friend: true,
+      uid: 2
+    };
+    let usuario3: User = {
+      nick: 'Carlos',
+      subnick: 'Mi mensaje personal',
+      age: 28,
+      email: 'carlos@carlos.com',
+      friend: true,
+      uid: 3
+    };
+    let usuario4: User = {
+      nick: 'Marcos',
+      subnick: 'Mi mensaje personal',
+      age: 28,
+      email: 'marcos@marcos.com',
+      friend: true,
+      uid: 4
+    };
+    let usuario5: User = {
+      nick: 'Juan',
+      subnick: 'Mi mensaje personal',
+      age: 28,
+      email: 'juan@juan.com',
+      friend: true,
+      uid: 5
+    };
+    this.friends = [usuario1, usuario2, usuario3, usuario4, usuario5];
   }
 
   ngOnInit() {
